ba90076e43a79484f72f4ed386bb2079eed6115a
LKWLaLa <lkwlala@gmail.com>
2016-11-30 17:43:21 -0500

    add bower.json and install bower_components.  Add angular to js manifest file.

--- angular-rails-app/app/assets/javascripts/application.js
+++ angular-rails-app/app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
+//= require angular-rails-templates
+//= require angular-ui-router
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
