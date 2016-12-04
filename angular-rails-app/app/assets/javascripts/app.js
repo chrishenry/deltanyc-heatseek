e8bb31632e6f77b94cc185843073ed2fca545594
LKWLaLa <lkwlala@gmail.com>
2016-12-04 18:43:25 -0500

    use angular-ui-bootstrap for typeahead directive

--- angular-rails-app/app/assets/javascripts/app.js
+++ angular-rails-app/app/assets/javascripts/app.js
@@ -1,5 +1,5 @@
 angular
-    .module('app', ['ui.router', 'templates'])
+    .module('app', ['ui.router', 'templates', 'ui.bootstrap'])
     .config(function ($stateProvider, $urlRouterProvider) {
 
       $stateProvider
