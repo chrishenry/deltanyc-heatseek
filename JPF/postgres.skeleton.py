3454327add267542c702d6c79cbb6366e74bfb93
jpfreeley <jpf321@gmail.com>
2016-12-04 11:40:22 -0500

    Added dtype to PLUTO glob

--- JPF/postgres.skeleton.py
+++ JPF/postgres.skeleton.py
@@ -1054,7 +1054,7 @@ if process_file == "PLUTO" or process_file == "ALL":
 	pluto_data = pd.DataFrame()
 	pluto_list_ = []
 	for file_ in allFiles: #iterate through all csv files and create a pandas df
-	    pluto_df = pd.read_csv(file_,index_col=None, header=0, dtype=dtype_dict)
+	    pluto_df = pd.read_csv(file_,index_col=None, header=0, dtype=PLUTO_dtype_dict)
 	    pluto_list_.append(pluto_df) #append every df to a big list
 	pluto_data = pd.concat(pluto_list_) #combine the big list into one big pandas df
 	pluto_data = pluto_data.reset_index(drop=True)
