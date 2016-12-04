c02cc63380412b21be8297327e097a1e749294f6
LKWLaLa <lkwlala@gmail.com>
2016-12-03 20:22:25 -0500

    add attributes to properties serializer.  Add has_many associations on properties model

--- angular-rails-app/app/serializers/property_serializer.rb
+++ angular-rails-app/app/serializers/property_serializer.rb
@@ -1,3 +1,5 @@
 class PropertySerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :street_address, :city, :state, :zipcode, 
+  :total_units, :bbl, :bin, :rent_stabilized, :owner_id
+
 end
