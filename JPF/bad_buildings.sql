ce864c4a3052d6c700f1bb3781a4b933e3c2dee3
jpfreeley <jpf321@gmail.com>
2016-12-05 12:15:10 -0500

    Final Import and bad_building.sql + AEP csv file for import.

--- JPF/bad_buildings.sql
+++ JPF/bad_buildings.sql
@@ -34,6 +34,8 @@ alter table hpd_registrationContact add index(type);
 alter table hpd_registrationContact add index(registrationid);
 alter table `pubadv_worst_landlords` add index(bin);
 alter table pluto_nyc add index(bbl);
+alter table tb_changes_summary add index(ucbbl);
+alter table hpd_aep_list add index(full_addr);
 
 
 delete from dob_violations where boro not in ('1','2','3','4','5');
@@ -109,7 +111,7 @@ create temporary table
 	hpd_complaint_counts
 select 
 	concat(trim(hb.housenumber), " ", trim(hb.streetname), " ", trim(hb.boro)) hbaddress,
-	concat(boroid,LPAD(hb.block, 5, '0'),LPAD(hb.lot, 4, '0')) bbl,
+	concat(trim(boroid),trim(LPAD(hb.block, 5, '0')),trim(LPAD(hb.lot, 4, '0'))) bbl,
 	hb.buildingid as buildingid, 
 	hb.block as block,
 	hb.lot as lot,
@@ -128,7 +130,7 @@ left join
 on  
 	hb.buildingid = cc.buildingid;
 ALTER TABLE `hpd_complaint_counts` ADD INDEX (`buildingid`);
-alter table hpd_complaint_counts CHANGE bbl bbl int(12) NULL DEFAULT NULL;
+alter table hpd_complaint_counts CHANGE bbl bbl bigint(13) NULL DEFAULT NULL;
 ALTER TABLE `hpd_complaint_counts` ADD INDEX(bbl);
 ALTER TABLE `hpd_complaint_counts` ADD INDEX (hbaddress(255));
 
@@ -174,8 +176,7 @@ select
 	count(*) dob_violation_cnt
 from 
 	dob_violations dv
-left join boro_lookup bl
-on dv.boro = bl.boroid
+left join boro_lookup bl on dv.boro = bl.boroid
 group by 
 	1,2;
 alter table violation_counts add index(dobv_address(255));
@@ -312,11 +313,64 @@ alter table hpd_heat_complaints_311 add index (address_311);
 ################
 ################
 
+drop table if exists aep_2008;
+drop table if exists aep_2009;
+drop table if exists aep_2010;
+drop table if exists aep_2011;
+drop table if exists aep_2012;
+drop table if exists aep_2013;
+drop table if exists aep_2014;
+drop table if exists aep_2015;
+drop table if exists aep_2016; 
+create temporary table aep_2008 select * from hpd_aep_list where year = 2008;
+create temporary table aep_2009 select * from hpd_aep_list where year = 2009;
+create temporary table aep_2010 select * from hpd_aep_list where year = 2010;
+create temporary table aep_2011 select * from hpd_aep_list where year = 2011;
+create temporary table aep_2012 select * from hpd_aep_list where year = 2012;
+create temporary table aep_2013 select * from hpd_aep_list where year = 2013;
+create temporary table aep_2014 select * from hpd_aep_list where year = 2014;
+create temporary table aep_2015 select * from hpd_aep_list where year = 2015;
+create temporary table aep_2016 select * from hpd_aep_list where year = 2016;
+alter table aep_2008 add index(full_addr);
+alter table aep_2009 add index(full_addr);
+alter table aep_2010 add index(full_addr);
+alter table aep_2011 add index(full_addr);
+alter table aep_2012 add index(full_addr);
+alter table aep_2013 add index(full_addr);
+alter table aep_2014 add index(full_addr);
+alter table aep_2015 add index(full_addr);
+alter table aep_2016 add index(full_addr);
+
+################
+################
+
 drop table if exists bad_buildings;
 
 create table bad_buildings
 select 
 	cc.*, 
+	heat_311_hpd_count,
+	all_311_hpd_count,
+	if (a08.year = 2008,1,0) aep08,
+	if (a09.year = 2009,1,0) aep09,
+	if (a10.year = 2010,1,0) aep10,
+	if (a11.year = 2011,1,0) aep11,
+	if (a12.year = 2012,1,0) aep12,
+	if (a13.year = 2013,1,0) aep13,
+	if (a14.year = 2014,1,0) aep14,
+	if (a15.year = 2015,1,0) aep15,
+	if (a16.year = 2016,1,0) aep16,
+	(
+	if (a08.year = 2008,1,0)+
+	if (a09.year = 2009,1,0)+
+	if (a10.year = 2010,1,0)+
+	if (a11.year = 2011,1,0)+
+	if (a12.year = 2012,1,0)+
+	if (a13.year = 2013,1,0)+
+	if (a14.year = 2014,1,0)+
+	if (a15.year = 2015,1,0)+
+	if (a16.year = 2016,1,0)
+	) as total_aep_yrs,
 	pnyc.council,
 	pnyc.ct2010,
 	pnyc.cb2010,
@@ -326,8 +380,10 @@ select
 	pnyc.histdist,
 	pnyc.xcoord,
 	pnyc.ycoord,
-	heat_311_hpd_count,
-	all_311_hpd_count,
+	tbcs.unitsstab2007,
+	tbcs.unitsstab2015,
+	tbcs.diff, 
+	tbcs.percentchange,
 	hs_permit_cnt, 
 	permit_cnt,
 	dob_violation_cnt,
@@ -352,11 +408,11 @@ select
 from 
 	hpd_complaint_counts cc
 left join 
-	hs_permit_counts on hbaddress = hs_dobp_address
+	hs_permit_counts on (hbaddress) = (hs_dobp_address)
 left join 
-	permit_counts on hbaddress = dobp_address
+	permit_counts on (hbaddress) = (dobp_address)
 left join 
-	violation_counts on hbaddress = dobv_address
+	violation_counts on (hbaddress) = (dobv_address)
 left join 
 	pubadv_worst_landlords pawl on cc.bin = pawl.bin
 left join
@@ -370,16 +426,36 @@ left join
 left join
 	lit_count lc on cc.buildingid = lc.buildingid
 left join
-	hpd_heat_complaints_311 hhc3 on hbaddress = hhc3.address_311
+	hpd_heat_complaints_311 hhc3 on (hbaddress) = (hhc3.address_311)
 left join
-	hpd_complaints_311 hc3 on hbaddress = hc3.address_311
+	hpd_complaints_311 hc3 on (hbaddress) = (hc3.address_311)
 left join
 	pluto_nyc pnyc on cc.bbl = pnyc.bbl
 left join 
 	hpd_headOfficers ho on cc.buildingid = ho.buildingid
 left join 
 	hpd_corps hc on cc.buildingid = hc.buildingid
-		;
+left join
+	tb_changes_summary tbcs on tbcs.ucbbl = cc.bbl 
+left join
+	aep_2008 a08 on (a08.full_addr) = (hbaddress)
+left join
+	aep_2009 a09 on (a09.full_addr) = (hbaddress)
+left join
+	aep_2010 a10 on (a10.full_addr) = (hbaddress)
+left join
+	aep_2011 a11 on (a11.full_addr) = (hbaddress)
+left join
+	aep_2012 a12 on (a12.full_addr) = (hbaddress)
+left join
+	aep_2013 a13 on (a13.full_addr) = (hbaddress)
+left join
+	aep_2014 a14 on (a14.full_addr) = (hbaddress)
+left join
+	aep_2015 a15 on (a15.full_addr) = (hbaddress)		
+left join
+	aep_2016 a16 on (a16.full_addr) = (hbaddress)
+;
 
 ## DROP ALL KEYS AND CHANGES TO ORIGINAL TABLES RUN FAR ABOVE
 alter table hpd_buildings drop primary key;
@@ -396,7 +472,10 @@ alter table hpd_registrations drop index registrationid;
 alter table hpd_registrationContact drop index registrationcontactid;
 alter table hpd_registrationContact drop index registrationid;
 alter table dob_violations drop index boro;
-alter table `pubadv_worst_landlords` drop index bin;
+alter table pubadv_worst_landlords drop index bin;
+alter table pluto_nyc drop index bbl;
+alter table tb_changes_summary drop index ucbbl;
+alter table hpd_aep_list drop index full_addr;
 
 select * from bad_buildings limit 100;
 
@@ -420,3 +499,4 @@ select * from bad_buildings limit 100;
 -- alter table hpd_registrations DROP index registrationid ;
 -- alter table hpd_registrationContact drop index registrationid;
 
+select * from bad_buildings limit 100; 
