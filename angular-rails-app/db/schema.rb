13429ed95fc2863409fdbda30e132f08db465225
Chris Henry <henry.christopher@gmail.com>
2016-12-11 00:21:50 -0500

    fix: corporation column name

--- angular-rails-app/db/schema.rb
+++ angular-rails-app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211045751) do
+ActiveRecord::Schema.define(version: 20161211052000) do
 
   create_table "TEST_violations", id: false, force: :cascade do |t|
     t.integer  "isn_dob_bis_viol",     limit: 4
@@ -358,7 +358,7 @@ ActiveRecord::Schema.define(version: 20161211045751) do
     t.string   "zipcode",                     limit: 255
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
-    t.integer  "corpotation_name",            limit: 4
+    t.integer  "corporation_name",            limit: 4
     t.integer  "hpd_registration_id",         limit: 4
     t.integer  "hpd_registration_contact_id", limit: 4
     t.string   "hpd_type",                    limit: 255
