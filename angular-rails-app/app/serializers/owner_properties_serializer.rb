180a2d1b4f540e40c9731c5d633605c53503c852
Chris Henry <henry.christopher@gmail.com>
2016-12-27 16:54:18 -0500

    fix: Correct the owner property serializer

--- angular-rails-app/app/serializers/owner_properties_serializer.rb
+++ angular-rails-app/app/serializers/owner_properties_serializer.rb
@@ -1,6 +1,6 @@
 class OwnerPropertiesSerializer < ActiveModel::Serializer
-  attributes :id, :street_address, :city, :state, :zipcode, 
-  :total_units, :bbl, :bin, :rent_stabilized, :full_address
+  attributes :id, :street_address, :city, :state, :zipcode,
+  :total_units, :expand_borough, :block, :lot, :bin, :bin, :rent_stabilized, :full_address
 
-  belongs_to :owner 
+  belongs_to :owner
 end
