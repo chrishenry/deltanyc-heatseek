d4198ed70307ad94df712b0db8ae377d42ceb531
Chris Henry <henry.christopher@gmail.com>
2017-01-02 21:33:02 -0500

    new: Implement hpd complaint count, ensuring that we're limiting to open complaints

--- angular-rails-app/app/serializers/property_serializer.rb
+++ angular-rails-app/app/serializers/property_serializer.rb
@@ -1,6 +1,7 @@
 class PropertySerializer < ActiveModel::Serializer
   attributes :id, :street_address, :city, :state, :zipcode,
-  :total_units, :expand_borough, :block, :lot, :bin, :rent_stabilized, :full_address
+  :total_units, :expand_borough, :block, :lot, :bin, :rent_stabilized, :full_address,
+  :hpd_open_complaints_count
 
   has_many :dob_permits
   has_many :dob_violations
@@ -8,12 +9,16 @@ class PropertySerializer < ActiveModel::Serializer
   has_many :litigations
   has_many :owners
 
+  def hpd_open_complaints_count
+    @object.hpd_complaints.where(status: 'OPEN').size
+  end
+
   has_many :complaint311s do
     @object.complaint311s.limit(5)
   end
 
   has_many :hpd_complaints do
-    @object.hpd_complaints.limit(5)
+    @object.hpd_complaints.where(status: 'OPEN').limit(5)
   end
 
 end
