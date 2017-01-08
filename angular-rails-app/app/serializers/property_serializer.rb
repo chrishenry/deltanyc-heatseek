984f2cd99a5af9327a1592e24b0d0f946d9a242a
Chris Henry <henry.christopher@gmail.com>
2017-01-07 19:08:07 -0500

    new: Implement 311

--- angular-rails-app/app/serializers/property_serializer.rb
+++ angular-rails-app/app/serializers/property_serializer.rb
@@ -1,10 +1,16 @@
 class PropertySerializer < ActiveModel::Serializer
   attributes :id, :street_address, :city, :state, :zipcode,
   :total_units, :expand_borough, :block, :lot, :bin, :rent_stabilized, :full_address,
-  :hpd_open_complaints_count, :dob_permits_count, :dob_violations_count, :litigations_count
+  :hpd_open_complaints_count, :dob_permits_count, :dob_violations_count, :litigations_count,
+  :complaint311s_count
 
   has_many :hpd_violations
   has_many :owners
+
+
+  def complaint311s_count
+    @object.complaint311s.size
+  end
 
   def litigations_count
     @object.hpd_complaints.size
