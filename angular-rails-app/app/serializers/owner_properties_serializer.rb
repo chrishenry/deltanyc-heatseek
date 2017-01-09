efa515eb2b2b4b50e5515711bebaf1f1783d2c1c
Chris Henry <henry.christopher@gmail.com>
2017-01-08 23:04:36 -0500

    fix: Remove duplicate bin in owners

--- angular-rails-app/app/serializers/owner_properties_serializer.rb
+++ angular-rails-app/app/serializers/owner_properties_serializer.rb
@@ -1,6 +1,6 @@
 class OwnerPropertiesSerializer < ActiveModel::Serializer
   attributes :id, :street_address, :city, :state, :zipcode,
-  :total_units, :expand_borough, :block, :lot, :bin, :bin, :rent_stabilized, :full_address
+  :total_units, :expand_borough, :block, :lot, :bin, :rent_stabilized, :full_address
 
   belongs_to :owner
 end
