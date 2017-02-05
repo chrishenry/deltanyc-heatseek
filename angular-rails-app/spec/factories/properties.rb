cd8ecf736b4db94acdae6e15b583ec3f1b8a5975
LKWLaLa <lkwlala@gmail.com>
2017-02-05 14:01:50 -0500

    add factories folder and specs for Property model

---
+++ angular-rails-app/spec/factories/properties.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :property do |f|
+    f.street_address{FFaker::AddressUS.street_address} 
+    f.city {FFaker::AddressUS.city}
+    f.state {FFaker::AddressUS.state_abbr}
+    f.zipcode {FFaker::AddressUS.zip_code}
+    f.total_units {rand(5..200)}
+    f.rent_stabilized {FFaker::Boolean.random}
+  end
+end
+
