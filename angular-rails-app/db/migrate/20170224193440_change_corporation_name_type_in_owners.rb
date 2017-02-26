d408eccfdc46c3952543a8372e0ca2c4bfdbbeab
LKWLaLa <lkwlala@gmail.com>
2017-02-26 14:36:45 -0500

    remove migration - this has been done by Chris

--- angular-rails-app/db/migrate/20170224193440_change_corporation_name_type_in_owners.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCorporationNameTypeInOwners < ActiveRecord::Migration
-  def self.up
-    change_column :owners, :corporation_name, :string
-  end
- 
-  def self.down
-    change_column :owners, :corporation_name, :integer
-  end
-end
