2b9c48925407666570e802850de16b81e8234742
Chris Henry <henry.christopher@gmail.com>
2017-03-04 14:36:03 -0500

    chg: Rejigger how factory girl is required

--- angular-rails-app/spec/rails_helper.rb
+++ angular-rails-app/spec/rails_helper.rb
@@ -6,8 +6,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'factory_girl'
-require 'factories/properties'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
