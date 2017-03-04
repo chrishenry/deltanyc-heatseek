ddf7f2ef3115522d635aa9db83da83ecc758d6b7
Chris Henry <henry.christopher@gmail.com>
2017-03-03 23:20:40 -0500

    chg: undo some bad debug work

--- angular-rails-app/spec/models/property_spec.rb
+++ angular-rails-app/spec/models/property_spec.rb
@@ -8,14 +8,7 @@ RSpec.describe Property, type: :model do
   it "returns a property's full address as a string" do
     property = FactoryGirl.create(:property, street_address: "2143 N. Shore Dr.",
     city: "Holland", state: "MI", zipcode: "49508")
-    puts "HERERERER property.full_address"
-    puts property.full_address
-    Rails.logger.debug variable.inspect property
-    Rails.logger.debug variable.inspect property.full_address
-
-    sdgfs dfg
-
-    expect(property.full_adess).to eq("2143 N. Shore Dr., Holland, MI 49508")
+    expect(property.full_address).to eq("2143 N. Shore Dr., Holland, MI 49508")
   end
 
    it "returns a property's expanded borough name" do
