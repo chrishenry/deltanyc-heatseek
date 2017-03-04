080db6d138b80cd21585be50aa5bf3cdbb9fe5ab
Chris Henry <henry.christopher@gmail.com>
2017-03-03 23:05:32 -0500

    chg: Some fixes to get tests working

--- angular-rails-app/spec/models/property_spec.rb
+++ angular-rails-app/spec/models/property_spec.rb
@@ -4,16 +4,23 @@ RSpec.describe Property, type: :model do
   it "has a valid factory" do
      expect(FactoryGirl.create(:property)).to be_instance_of(Property)
   end
-  
+
   it "returns a property's full address as a string" do
-    property = FactoryGirl.create(:property, street_address: "2143 N. Shore Dr.", 
+    property = FactoryGirl.create(:property, street_address: "2143 N. Shore Dr.",
     city: "Holland", state: "MI", zipcode: "49508")
-    expect(property.full_address).to eq("2143 N. Shore Dr., Holland, MI 49508") 
+    puts "HERERERER property.full_address"
+    puts property.full_address
+    Rails.logger.debug variable.inspect property
+    Rails.logger.debug variable.inspect property.full_address
+
+    sdgfs dfg
+
+    expect(property.full_adess).to eq("2143 N. Shore Dr., Holland, MI 49508")
   end
 
    it "returns a property's expanded borough name" do
      property = (FactoryGirl.create(:property, borough: "BK"))
-     expect(property.expand_borough).to eq("BROOKLYN") 
+     expect(property.expand_borough).to eq("BROOKLYN")
   end
 
 end
--- angular-rails-app/spec/rails_helper.rb
+++ angular-rails-app/spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'factory_girl'
+require 'factories/properties'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -55,4 +57,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+  config.include FactoryGirl::Syntax::Methods
+
+
 end
