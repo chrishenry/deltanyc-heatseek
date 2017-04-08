489801c9c3ae33898924b089746acd511c6f7ada
LKWLaLa <lkwlala@gmail.com>
2017-04-08 18:13:01 -0400

    add routes for associations

--- angular-rails-app/config/routes.rb
+++ angular-rails-app/config/routes.rb
@@ -1,58 +1,37 @@
 Rails.application.routes.draw do
-  resources :properties
-  get '/query' => 'properties#query'
-  resources :owners
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
   root 'home#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :properties
+  get '/query' => 'properties#query'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :owners  
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :complaint311s, only: :index do
+    get 'search', on: :collection
+  end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  resources :dob_permits, only: :index do
+    get 'search', on: :collection
+  end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  resources :dob_violations, only: :index do
+    get 'search', on: :collection
+  end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  resources :hpd_complaints, only: :index do
+    get 'search', on: :collection
+  end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  resources :hpd_violations, only: :index do
+    get 'search', on: :collection
+  end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-end
+  resources :litigations, only: :index do
+    get 'search', on: :collection
+  end
+
+
+
+
+  
