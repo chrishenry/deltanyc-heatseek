e3bf83f6e069e04cca8cc55673d4eefe2a84d5c7
Chris Henry <henry.christopher@gmail.com>
2017-04-11 23:25:40 -0400

    Ensure exit 0 in both cases

--- docker/mysql-udf.sh
+++ docker/mysql-udf.sh
@@ -9,8 +9,6 @@ sed -i 's/USE mysql;//g' $FILE
 
 MYSQL_CMD="mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD"
 
-echo $MYSQL_CMD
-
 until $MYSQL_CMD -e ";"; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
@@ -19,11 +17,14 @@ done
 echo "MySQL is available..."
 
 echo "Checking if functions exist..."
-if $MYSQL_CMD -e "select LIB_MYSQLUDF_PREG_INFO();"; then
-  echo "Functions already added"
-else
+exists=`$MYSQL_CMD -N -e "select LIB_MYSQLUDF_PREG_INFO();" 2>/dev/null` || true
+
+# if exists command came back empty
+if [[ -z $exists ]]; then
   echo "Adding functions..."
   $MYSQL_CMD deltanyc < $FILE
   echo "UDFs added successfully"
+else
+  echo "Functions already added"
 fi
 
