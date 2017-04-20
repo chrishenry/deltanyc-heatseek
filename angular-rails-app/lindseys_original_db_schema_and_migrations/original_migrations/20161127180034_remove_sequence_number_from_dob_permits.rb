c7360b557c1ff6020c19726b68e42fcc5631a1b2
rdlester <sursiks.010@gmail.com>
2017-04-20 10:21:02 -0400

    remove old rails db spec

--- angular-rails-app/lindseys_original_db_schema_and_migrations/original_migrations/20161127180034_remove_sequence_number_from_dob_permits.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSequenceNumberFromDobPermits < ActiveRecord::Migration
-  def change
-    remove_column :dob_permits, :permit_sequence_num
-  end
-end
--- angular-rails-app/lindseys_original_db_schema_and_migrations/original_migrations/20161127180352_remove_community_board_from_dob_permits.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCommunityBoardFromDobPermits < ActiveRecord::Migration
-  def change
-    remove_column :dob_permits, :community_board
-  end
-end
