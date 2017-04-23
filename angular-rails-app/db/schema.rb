aafcd232360359efede95ea0efd46870de8ead16
rdlester <sursiks.010@gmail.com>
2017-04-22 23:55:45 -0400

    Add bbl to r_properties
    Remove duplicated work from properties connector task

--- angular-rails-app/db/schema.rb
+++ angular-rails-app/db/schema.rb
@@ -141,10 +141,12 @@ ActiveRecord::Schema.define(version: 20170225185334) do
     t.string   "borough",             limit: 255
     t.integer  "block",               limit: 4
     t.integer  "lot",                 limit: 4
+    t.integer  "bbl",                 limit: 8
     t.integer  "hpd_registration_id", limit: 4
   end
 
   add_index "properties", ["borough", "block", "lot"], name: "index_r_properties_on_borough_and_block_and_lot", unique: true, using: :btree
+  add_index "properties", ["bbl"], name: "index_r_properties_on_bbl", using: :btree
   add_index "properties", ["street_address", "zipcode"], name: "index_r_properties_on_street_address_and_zipcode", using: :btree
 
 end
